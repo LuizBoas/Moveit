481de1e77f93d63f82122d1c647682bc3ac3d792
Luiz Boas <luiz.boas@ccc.ufcg.edu.br>
2021-03-02 22:14:25 -0300

    progress in components,home

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+
+export function Profile(){
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/diego3g.png" alt="Luiz Fernando"/>
+            <div>
+                <strong>Luiz Fernando</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1</p>
+            </div>
+        </div>
+    )
+
+}
