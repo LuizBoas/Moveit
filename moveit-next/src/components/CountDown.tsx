adef0365058d442765014100de5ad21549cd7cc8
Luiz Boas <luiz.boas@ccc.ufcg.edu.br>
2021-03-20 11:39:59 -0300

    finalizando cronometro

--- moveit-next/src/components/CountDown.tsx
+++ moveit-next/src/components/CountDown.tsx
@@ -1,50 +1,74 @@
 import { useState, useEffect } from 'react'
 import styles from '../styles/components/CountDown.module.css'
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function CountDown() {
-    const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
 
-    const [minuteLeft, miniutesRight] = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+  const [minuteLeft, miniutesRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-    function startCountDown() {
-        setActive(true)
-    };
+  function startCountDown() {
+    setIsActive(true)
+  }
 
-    useEffect(()=> {
-        if(active && time > 0) {
-            setTimeout(() => {
-                setTime(time - 1);
-            }, 1000)
-        }
-        
-    }, [active, time])
+  function resetCountDown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1*60);
 
-    return (
+  }
+
+  useEffect(() => {// tem delay de 1 segundo para fazer as alterações
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    } else if (isActive && time === 0){
+      setHasFinished(true);
+      setIsActive(false);
+    } 
+  }, [isActive, time])
+
+  return (
+    <div>
+      <div className={styles.countDownContainer}>
         <div>
-            <div className={styles.countDownContainer}>
-                <div>
-                    <span>{minuteLeft}</span>
-                    <span>{miniutesRight}</span>
-                </div>
-                <span></span>
-                <div>
-                    <span>{secondLeft}</span>
-                    <span>{secondRight}</span>
-                </div>
-            </div>
-
-            <button
-                type="button"
-                className={styles.countDownButton}
-                onClick={startCountDown}
-            >
-                Iniciar um ciclo
-            </button>
+          <span>{minuteLeft}</span>
+          <span>{miniutesRight}</span>
         </div>
-    )
+        <span></span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      {hasFinished ? (
+        <button disabled type="button" className={styles.countDownButton}>
+          Ciclo encerrado
+        </button>
+      ): (
+        <>
+        {isActive ? (
+        <button type="button" className={`${styles.countDownButton} ${styles.countDownButtonActive}`} onClick={resetCountDown}>
+          Abandonar ciclo
+        </button>
+      ) : (
+        <button type="button" className={styles.countDownButton} onClick={startCountDown}>
+          Iniciar ciclo
+        </button>
+      )}
+        </>
+      )}
+
+      
+    </div>
+  )
 }
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,32 +1,26 @@
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { CountDown } from "../components/CountDown";
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Profile";
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { CountDown } from '../components/CountDown'
+import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
 
-import Head from 'next/head';
-import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head'
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Início | move.it</title>  
-      </Head>      
-      <ExperienceBar/>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
 
       <section>
         <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <CountDown/>
-
-        
-
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
         </div>
-        <div>
-
-        </div>
-
+        <div></div>
       </section>
     </div>
   )
